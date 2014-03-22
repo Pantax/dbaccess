bf0b88a994a85cfaec5b1b2bfc4a4d879f89654d
levoovoo <ozlevka@lev-ubuntu-desktop.(none)>
2014-03-22 21:18:20 +0200

    Finish CRUD for doctor and patient

--- test/common.js
+++ test/common.js
@@ -3,5 +3,38 @@
  */
 
 var dbmanager = require('../lib/dbmanager');
+var util = require('util');
+//dbmanager.createSchema();
 
-dbmanager.createSchema();
+
+/*dbmanager.saveDoctor({name:'levka pururjns', degree:'Doctor of mmmmmmmmm', id : 7}, function(err, result){
+    if(err) console.error(err);
+    else {
+        console.log('OK');
+    }
+
+    dbmanager.releasePool();
+});*/
+
+/* dbmanager.deleteDoctor(7, function(err, results){
+     if(err) console.error(err);
+     else console.log(results);
+    dbmanager.releasePool();
+ })*/
+
+/*
+dbmanager.savePatient({name : 'Hello morkovka', id : 2}, function(err, patient){
+    if(!err) {
+        console.log(patient);
+    }
+
+    dbmanager.releasePool();
+})*/
+
+dbmanager.deletePatient(2, function(err, result) {
+    if(!err) {
+        console.log(result);
+    }
+
+    dbmanager.releasePool();
+})
