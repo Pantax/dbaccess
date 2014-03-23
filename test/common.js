0e63dbe4a60a1deb9c730a2b3f96cc89594bea97
levoovoo <ozlevka@lev-ubuntu-desktop.(none)>
2014-03-23 12:10:00 +0200

    Create and test document CRUD

--- test/common.js
+++ test/common.js
@@ -4,37 +4,12 @@
 
 var dbmanager = require('../lib/dbmanager');
 var util = require('util');
-//dbmanager.createSchema();
 
+var mysql = require('mysql');
 
-/*dbmanager.saveDoctor({name:'levka pururjns', degree:'Doctor of mmmmmmmmm', id : 7}, function(err, result){
+dbmanager.deleteDocument(3,function(err, results){
     if(err) console.error(err);
-    else {
-        console.log('OK');
-    }
-
-    dbmanager.releasePool();
-});*/
-
-/* dbmanager.deleteDoctor(7, function(err, results){
-     if(err) console.error(err);
-     else console.log(results);
-    dbmanager.releasePool();
- })*/
-
-/*
-dbmanager.savePatient({name : 'Hello morkovka', id : 2}, function(err, patient){
-    if(!err) {
-        console.log(patient);
-    }
-
-    dbmanager.releasePool();
-})*/
-
-dbmanager.deletePatient(2, function(err, result) {
-    if(!err) {
-        console.log(result);
-    }
+    else console.log(results);
 
     dbmanager.releasePool();
 })
