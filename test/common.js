3c2807f0c94dd8d863948a898feb3e7bb47aa2ef
levoovoo <ozlevka@lev-ubuntu-desktop.(none)>
2014-03-25 15:58:08 +0200

    Start create dbmanager for real API

--- test/common.js
+++ test/common.js
@@ -6,6 +6,7 @@ var dbmanager = require('../lib/dbmanager');
 var util = require('util');
 var config = require('../config/config');
 var mysql = require('mysql');
+var fs = require('fs');
 
 function processCreate() {
     dbmanager.createSchema(function(err) {
@@ -13,6 +14,38 @@ function processCreate() {
     });
 }
 
+function insertDoctorsFromExample() {
+    var doctors = require('../config/doctorsexample');
+    dbmanager.loadDoctorsFromJson(doctors,function(err, results) {
+        if(err) console.error(err);
+        else console.log(results);
+
+        dbmanager.releasePool();
+    });
+}
+
+function createCategory(categoryName) {
+    dbmanager.insertCategory(categoryName, function(err, results) {
+        if(err) console.error(err);
+        else console.log(results);
+
+        dbmanager.releasePool();
+    })
+}
+
+function addDoctorToCategoryTest() {
+    dbmanager.addDoctorToCategory({id:1},{id : 1}, function(err, results){
+        if(err) console.error(err);
+        else console.log(results);
+
+        dbmanager.releasePool();
+    })
+}
+
+addDoctorToCategoryTest();
 
 
+//createCategory('Cardiologist');
+//insertDoctorsFromExample();
+//processCreate();
 
