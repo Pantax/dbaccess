03affa68bd196a5ba5f689c61fac1b2caf6a0ed4
levoovoo <ozlevka@lev-ubuntu-desktop.(none)>
2014-03-30 11:15:04 +0300

    add set config function add create doctor_appointment_options table create sql

--- config/createschema.sql
+++ config/createschema.sql
@@ -1,4 +1,4 @@
 CREATE TABLE pantax.doctors ( `id` INT UNSIGNED AUTO_INCREMENT NOT NULL,  `name` VARCHAR(256) NOT NULL, `prof_stat` TEXT NOT NULL, `pract_name` VARCHAR(256), `picture_url` VARCHAR(512), PRIMARY KEY (ID)) ENGINE = InnoDB ROW_FORMAT = DEFAULT;
 CREATE TABLE pantax.category (`id` INT UNSIGNED AUTO_INCREMENT NOT NULL, `category_name` VARCHAR(512) NOT NULL, PRIMARY KEY (ID)) ENGINE = InnoDB ROW_FORMAT = DEFAULT;
 CREATE TABLE pantax.doctor_category (`doctor_id` INT UNSIGNED NOT NULL, `category_id` INT UNSIGNED NOT NULL, PRIMARY KEY (doctor_id, category_id)) ENGINE = InnoDB ROW_FORMAT = DEFAULT;
-
+CREATE TABLE pantax.doctor_appointment_options ( `id` INT UNSIGNED AUTO_INCREMENT NOT NULL, `doctor_id` INT UNSIGNED NOT NULL, `from_time` TIME NOT NULL, `to_time` TIME NOT NULL,  PRIMARY KEY (id)) ENGINE = InnoDB ROW_FORMAT = DEFAULT;
